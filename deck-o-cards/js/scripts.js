9a37cc09c28df524af8bc78e699c1ee33f3b0435
Bang Nguyen <bangnv77@gmail.com>
2016-06-07 21:44:22 -0700

    add for each function to have a deck of cards

--- deck-o-cards/js/scripts.js
+++ deck-o-cards/js/scripts.js
@@ -1,9 +1,27 @@
-
-var suits = ['spades', 'hearts', 'clubs', 'diamonds'];
-var ranks = ['ace', 2, 3, 4, 5, 6, 7, 8, 9, 10,'jack', 'queen', 'king'];
-
-suits.forEach(function(suit) {
-  cards.forEach(function(card) {
-    $('ul').append('<li>' + card + ' of ' + suit + '</li>');
+$(function () {
+  var cardValues = ['A',"2","3",'4','5','6','7','8','9','10','J','Q','K'];
+  var cardSuits = ["Clubs","Spades","Diamonds","Hearts"];
+  var cardDeck = [];
+  cardSuits.forEach(function(cardSuit){
+    cardValues.forEach(function(cardValue){
+      cardDeck.push(cardValue + " of " + cardSuit);
+    });
+  });
+  cardDeck.forEach(function(card){
+    document.write("<li>"+card+"</li>");
   });
 });
+
+
+
+//
+// var suits = ['spades', 'hearts', 'clubs', 'diamonds'];
+// var ranks = ['ace', 2, 3, 4, 5, 6, 7, 8, 9, 10,'jack', 'queen', 'king'];
+//
+// suits.forEach(function(suit) {
+//   cards.forEach(function(card) {
+//     $('ul').append('<li>' + card + ' of ' + suit + '</li>');
+//     // $('#deck').show();
+//     // $('.btn').hide();
+//   });
+// });
