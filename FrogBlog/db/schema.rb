cdd00cc7b2b25af46f6e6d7e6cac8739a5838f70
Bang Nguyen <bangnv77@gmail.com>
2016-09-03 00:19:46 -0700

    display 3 different images for three posts

--- FrogBlog/db/schema.rb
+++ FrogBlog/db/schema.rb
@@ -10,15 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160903045834) do
+ActiveRecord::Schema.define(version: 20160903070940) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
     t.text     "description"
     t.string   "slug"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+    t.string   "banner_image_url"
     t.index ["slug"], name: "index_posts_on_slug", unique: true
   end
 
